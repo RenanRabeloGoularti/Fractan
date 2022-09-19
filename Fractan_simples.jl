ca0a5108644ad4b7f91445b4c57cd0272545e326
RenanRabeloGoularti <80898566+RenanRabeloGoularti@users.noreply.github.com>
2022-09-19 18:01:10 -0300

    Update Fractan_simples.jl

--- Fractan_simples.jl
+++ Fractan_simples.jl
@@ -2,7 +2,7 @@ using Images # Importa o pacote para criação de imagens
 
 function principal()::Nothing
     resolucao::NTuple{2, Int64} = (512, 512) # Tamanho da imagem em pixels
-    nome::String = "Curva de Levy" # Nome do arquivo que será salvo
+    nome::String = "Curva de Lévy" # Nome do arquivo que será salvo
     iteracoes::Int64 = 20 # Tamanho da árvore de pontos (cuidado, aumenta exponencialmente)
     fractal::Vector{NTuple{4, Float64}} = 
     [
@@ -13,50 +13,52 @@ function principal()::Nothing
     # A primeira entrada é o valor de 'k*cos(theta)' e a segunda 'k*sin(theta)'
     # Não se confunda com o que está no relatório!
 
-    densidade::Matrix{Int64} = zeros(Int64, resolucao) # Frequência dos pontos
-
+    imagem::Matrix{N0f8} = zeros(N0f8, resolucao)
     fator::Float64 = Float64(min(resolucao[1], resolucao[2])) # Necessário para marcar os pontos
     nF::Int64 = length(fractal)
 
-    p::Int64 = 2 # Posição do ponteiro (não é um ponteiro de memória)
-    posicao::Vector{Int64} = ones(Int64, iteracoes) # Armazena qual o endereço do ponto que está sendo marcado
-    pontos::Matrix{Float64} = zeros(Float64, (iteracoes, 2)) # Armazena os pontos calculados
+    posicao::Vector{Int64} = ones(Int64, iteracoes) # Armazena qual o endereço do ponto que será calculado
+    ponto::Vector{Float64} = [0.0, 0.0]
 
-    while p > 1 
-        if p > iteracoes # Caso estamos indo além da profundidade que queríamos
-            p -= 1 # Retorna uma posição
-            x::Int64 = trunc(Int64, fator*(pontos[p,1]) + Float64(resolucao[2])*0.5)
-            y::Int64 = trunc(Int64, fator*(-pontos[p,2]) + Float64(resolucao[1])*0.5)
-            # Mapeia os pontos para a imagem
-            if x >= 1 && x <= resolucao[2] && y >= 1 && y <= resolucao[1]
-                densidade[y,x] += 1
-            end # Marca onde o ponto cai
-            posicao[p] += 1 # Vai para a próximo ponto
-        elseif posicao[p] > nF # Se estivermos tentando calcular uma função que não existe
-            posicao[p] = 1 # Volte para a primeira
-            p -= 1
-            posicao[p] += 1
-        else
-            pontos[p, 1] = fractal[posicao[p]][1]*pontos[p - 1, 1] + fractal[posicao[p]][2]*pontos[p - 1, 2] + fractal[posicao[p]][3]
-            pontos[p, 2] = fractal[posicao[p]][1]*pontos[p - 1,2] - fractal[posicao[p]][2]*pontos[p - 1, 1] + fractal[posicao[p]][4]
-            # Calcula o ponto baseado nas funções e armazena
-            p += 1 # Avança uma posição
-        end
-    end
+    for n::Int64 in 1:(nF^iteracoes)
+        
+        for i::Int64 in posicao
+            h::Float64 = ponto[1]
+            ponto[1] = fractal[i][1]*ponto[1] + fractal[i][2]*ponto[2] + fractal[i][3]
+            ponto[2] = fractal[i][1]*ponto[2] - fractal[i][2]*h + fractal[i][4]
+        end  # Calcula o valor de ponto baseado na expansão
 
-    imagem::Matrix{N0f8} = zeros(N0f8, resolucao)
+        x::Int64 = trunc(Int64, fator*(ponto[1]) + Float64(resolucao[2])*0.5)
+        y::Int64 = trunc(Int64, fator*(-ponto[2]) + Float64(resolucao[1])*0.5)
+        # Mapeia os pontos para a imagem
 
-    nitidez::Float64 = 1.0/maximum(densidade) # Necessário para o fractal ficar nítido
-
-    for y::Int64 in 1:resolucao[1] 
-        for x::Int64 in 1:resolucao[2]
-            if densidade[y,x]*nitidez <= 1.0
-                imagem[y,x] = N0f8(Float64(densidade[y,x])*nitidez)
-            else
+        if x >= 1 && x <= resolucao[2] && y >= 1 && y <= resolucao[1]
+            if imagem[y,x] + 0.004 > 1.0
                 imagem[y,x] = 1.0
+            else
+                imagem[y,x] += 0.004
             end
-        end
-    end  # Marca os pixels na imagem com a sua nitidez baseada na frequência de pontos 
+        end # Marca onde o ponto cai
+
+        p::Int64 = iteracoes
+
+        posicao[p] += 1
+
+        while true
+            if p == 0
+                break
+            elseif posicao[p] == nF + 1
+                posicao[p] = 1
+                p -= 1
+                if p == 0
+                    break
+                end
+                posicao[p] += 1
+            else
+                break
+            end
+        end # Calcula a expansão do número sem redundâncias
+    end
 
     save("$(nome).png", imagem)
 
